40336d38519ba51b2c18dd2bfdcfba0db5d91258
Siddhant Mishra <siddhant48311@gmail.com>
2020-07-18 03:32:49 +0530

    Updates

---
+++ precache-manifest.2e4ca5bf723b7208256bffad4f5634cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1422d0fe80ae82d40b6a30a162b6b053",
+    "url": "//breaking-bad-cast/index.html"
+  },
+  {
+    "revision": "24c787b64a1b35c95219",
+    "url": "//breaking-bad-cast/static/css/main.65ee64e9.chunk.css"
+  },
+  {
+    "revision": "f5dedf54af2bc3cf0eba",
+    "url": "//breaking-bad-cast/static/js/2.a081320a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//breaking-bad-cast/static/js/2.a081320a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24c787b64a1b35c95219",
+    "url": "//breaking-bad-cast/static/js/main.ac1a799a.chunk.js"
+  },
+  {
+    "revision": "676938bf694975fb6450",
+    "url": "//breaking-bad-cast/static/js/runtime-main.37c86601.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "//breaking-bad-cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "//breaking-bad-cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "//breaking-bad-cast/static/media/spinner.235b9b3e.gif"
+  }
+]);
